7edec38c07dbc9ccfd7ea0379fe506aac6e8082e
Joshua Alletto <joshalletto@gmail.com>
2018-08-31 21:04:42 -0500

    add code from last challenge

--- classes/school.rb
+++ classes/school.rb
@@ -10,5 +10,13 @@ class School
         @students = Student.all
         @staff = Staff.all  
     end   
+    
+    def list_students
+        students.each_with_index { |s , i| puts "#{i + 1}. #{s.name} #{s.school_id}\n" }
+    end 
+
+    def find_student_by_id(id)
+        students.find { |student| student.school_id == id}
+    end 
 end 
 
--- runner.rb
+++ runner.rb
@@ -2,5 +2,19 @@ require_relative './classes/school.rb'
 
 school = School.new('Ridgemont High', '1212 Main st.')
 
-p school.students 
-p school.staff 
+mode = nil 
+until mode == '5'
+    puts "\nWhat would you like to do?\nOptions:\n1. List All Students\n2. View Individual Student <student_id>\n3. Add a Student\n4. Remove a Student <student_id>\n5. Quit"
+
+    mode = gets.chomp
+    puts '' 
+    case mode 
+    when '1'
+        school.list_students
+    when '2'
+        puts 'Enter student id:'
+        puts school.find_student_by_id(gets.chomp)          
+    when '5' 
+        return 
+    end  
+end 
