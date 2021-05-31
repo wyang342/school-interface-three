fc35adc9a4802f09454bf02e43b56f1facab7274
Won Yang <won@gatech.edu>
2021-05-31 16:44:45 -0500

    adds solution

--- classes/school.py
+++ classes/school.py
@@ -1,5 +1,9 @@
+from posixpath import dirname
 from classes.staff import Staff
 from classes.student import Student
+import os
+import csv
+
 
 class School:
     def __init__(self, name):
@@ -16,3 +20,28 @@ class School:
         for student in self.students:
             if student.school_id == student_id:
                 return student
+
+    def add_student(self, student_data):
+        new_student = Student(**student_data)
+        self.students.append(new_student)
+        self.update_csv()
+
+    def remove_student(self, id):
+        for student in self.students:
+            if student.school_id == id:
+                self.students.remove(student)
+                break
+        self.update_csv()
+
+    def update_csv(self):
+        current_dir_name = os.path.abspath(os.path.dirname(__file__))
+        path_name = os.path.join(current_dir_name, "../data/students.csv")
+
+        with open(path_name, "w") as csvfile:
+            fields = ["name", "age", "role", "school_id", "password"]
+            csvwriter = csv.writer(csvfile)
+
+            csvwriter.writerow(fields)
+            for student in self.students:
+                csvwriter.writerow(
+                    [student.name, student.age, student.role, student.school_id, student.password])
--- runner.py
+++ runner.py
@@ -1,6 +1,6 @@
-from classes.school import School 
+from classes.school import School
 
-school = School('Ridgemont High') 
+school = School('Ridgemont High')
 
 while True:
     mode = input("\nWhat would you like to do?\nOptions:\n1. List All Students\n2. View Individual Student <student_id>\n3. Add a Student\n4. Remove a Student <student_id>\n5. Quit\n")
@@ -9,7 +9,18 @@ while True:
         school.list_students()
     elif mode == '2':
         student_id = input('Enter student id:')
-        student_string = str(school.find_student_by_id(student_id))
-        print(student_string)
+        student = school.find_student_by_id(student_id)
+        print(str(student))
+    elif mode == '3':
+        student_data = {'role': 'student'}
+        student_data['name'] = input('Enter student name:\n')
+        student_data['age'] = input('Enter student age: \n')
+        student_data['school_id'] = input('Enter student school id: \n')
+        student_data['password'] = input('Enter student password: \n')
+
+        school.add_student(student_data)
+    elif mode == '4':
+        student_id = input('Enter student id:')
+        school.remove_student(student_id)
     elif mode == '5':
         break
